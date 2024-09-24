cedb93561e845cd22ec5874246991346ddc1b083
Deranindu Gunasekara <140329900+DeraniDU@users.noreply.github.com>
2024-09-24 22:17:57 +0530

    Build Layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { AiOutlinePlus } from 'react-icons/ai';
+
+const style = {
+  bg: 'h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#56CCF2]'
+}
+
 
 function App() {
   return (
-    <div>
-      <h1> Hello world</h1>
+    <div className={style.bg}>
+      <div className={style.container}>
+        <h3 className={style.heading}>TO-DO APP</h3>
+        <form className={style.form}>
+          <input type="text" className={style.input} placeholder="Add a task" />
+          <button className={style.button}><AiOutlinePlus size={30}/> </button>
+        </form>
+        <ul>
+
+          
+        </ul>
+
+      </div>
 
     </div>
   );

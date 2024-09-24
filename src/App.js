3787efb1a036bdece24f1dfd902c04eca0996d1e
Deranindu Gunasekara <140329900+DeraniDU@users.noreply.github.com>
2024-09-25 01:19:38 +0530

    Create TODO component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React,{useState}from 'react';
 import { AiOutlinePlus } from 'react-icons/ai';
+import Todo from './Todo';
 
 const style = {
   bg: 'h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#56CCF2]'
 }
 
 
-function App() {
+function App(){
+  const [todos, setTodos] = useState(['Learn React', 'Learn Firebase', 'Learn Tailwind CSS']);
+
+
+
   return (
     <div className={style.bg}>
       <div className={style.container}>
@@ -16,14 +21,21 @@ function App() {
           <button className={style.button}><AiOutlinePlus size={30}/> </button>
         </form>
         <ul>
+          {todos.map((todo,index) => (
+            <Todo key={index} todo={todo} />
 
+
+          ))}
           
+
         </ul>
+
+        <p className={style.count}> You have 2 more Todos</p>
 
       </div>
 
     </div>
   );
-}
+          }
 
 export default App;

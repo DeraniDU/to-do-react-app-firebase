50b49af824b8efa3b379672a9604791bdbc0faf6
Deranindu Gunasekara <140329900+DeraniDU@users.noreply.github.com>
2024-09-25 13:24:53 +0530

    Add CSS

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import { AiOutlinePlus } from 'react-icons/ai';
 import Todo from './Todo';
 
 const style = {
-  bg: 'h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#56CCF2]'
+  bg: 'h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#56CCF2]',
+  container: 'bg-slate-100 max-w-[500px] w-full mx-auto p-4 rounded-lg shadow-lg',
+  heading : 'text-3xl font-bold text-center text gray-800 p-2',
+  form: 'flex justify-between',
+  input: 'border p-2 w-full text-xl',
+  button: 'border p-4 ml-2 bg-purple-500 text-slate-100',
+  count: 'text-center text-gray-800 p-2',
 }
 
 
--- src/Todo.jsx
+++ src/Todo.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import { FaRegTrashAlt } from "react-icons/fa"; 
 
 const style = {
-
-
+     li: 'flex justify-between bg-slate-200 p-4 my-2 capitalize rounded-lg',
+     row: 'flex',
+     text: 'ml-2 cursor-pointer',
+     textComplete: 'ml-2 cursor-pointer line-through',
+     button: 'cursor-pointer flex items-center'
 }
 
 const Todo = ({todo}) => {

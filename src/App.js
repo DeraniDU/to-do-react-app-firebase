0b1f4107fd8032cd5083ec66b4c8fd8a1ad172da
Deranindu Gunasekara <140329900+DeraniDU@users.noreply.github.com>
2024-09-26 11:14:12 +0530

    create CRUD functionality and Completed Application

--- src/App.js
+++ src/App.js
@@ -1,71 +1,102 @@
-import React,{useState,useEffect}from 'react';
+import React, { useState, useEffect } from 'react';
 import { AiOutlinePlus } from 'react-icons/ai';
 import Todo from './Todo';
-import {db} from './firebase';
-import {query,collection,onSnapshot
-
-
-
+import { db } from './firebase';
+import {
+  query,
+  collection,
+  onSnapshot,
+  updateDoc,
+  doc,
+  addDoc,
+  deleteDoc,
 } from 'firebase/firestore';
 
 const style = {
-  bg: 'h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#56CCF2]',
-  container: 'bg-slate-100 max-w-[500px] w-full mx-auto p-4 rounded-lg shadow-lg',
-  heading : 'text-3xl font-bold text-center text gray-800 p-2',
-  form: 'flex justify-between',
-  input: 'border p-2 w-full text-xl',
-  button: 'border p-4 ml-2 bg-purple-500 text-slate-100',
-  count: 'text-center text-gray-800 p-2',
-}
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
+  container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
+  heading: `text-3xl font-bold text-center text-gray-800 p-2`,
+  form: `flex justify-between`,
+  input: `border p-2 w-full text-xl`,
+  button: `border p-4 ml-2 bg-purple-500 text-slate-100`,
+  count: `text-center p-2`,
+};
 
-
-function App(){
+function App() {
   const [todos, setTodos] = useState([]);
-   
-  //create todo
-  //read from firebase
+  const [input, setInput] = useState('');
 
+  // Create todo
+  const createTodo = async (e) => {
+    e.preventDefault(e);
+    if (input === '') {
+      alert('Please enter todo');
+      return;
+    }
+    await addDoc(collection(db, 'todos'), {
+      text: input,
+      completed: false,
+    });
+    setInput('');
+  };
 
-
+  // Read todo from firebase
   useEffect(() => {
-    const q = query(collection(db, "todos"))
+    const q = query(collection(db, 'todos'));
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
       let todosArr = [];
       querySnapshot.forEach((doc) => {
-        todosArr.push({...doc.data(),id: doc.id});
+        todosArr.push({ ...doc.data(), id: doc.id });
       });
       setTodos(todosArr);
-    })
-    return () => {
-      unsubscribe();
-    }
-  },[]);
+    });
+    return () => unsubscribe();
+  }, []);
 
+  // Update todo in firebase
+  const toggleComplete = async (todo) => {
+    await updateDoc(doc(db, 'todos', todo.id), {
+      completed: !todo.completed,
+    });
+  };
+
+  // Delete todo
+  const deleteTodo = async (id) => {
+    await deleteDoc(doc(db, 'todos', id));
+  };
 
   return (
     <div className={style.bg}>
       <div className={style.container}>
-        <h3 className={style.heading}>TO-DO APP</h3>
-        <form className={style.form}>
-          <input type="text" className={style.input} placeholder="Add a task" />
-          <button className={style.button}><AiOutlinePlus size={30}/> </button>
+        <h3 className={style.heading}>Todo App</h3>
+        <form onSubmit={createTodo} className={style.form}>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            className={style.input}
+            type='text'
+            placeholder='Add Todo'
+          />
+          <button className={style.button}>
+            <AiOutlinePlus size={30} />
+          </button>
         </form>
         <ul>
-          {todos.map((todo,index) => (
-            <Todo key={index} todo={todo} />
-
-
+          {todos.map((todo, index) => (
+            <Todo
+              key={index}
+              todo={todo}
+              toggleComplete={toggleComplete}
+              deleteTodo={deleteTodo}
+            />
           ))}
-          
-
         </ul>
-
-        <p className={style.count}> You have 2 more Todos</p>
-
+        {todos.length < 1 ? null : (
+          <p className={style.count}>{`You have ${todos.length} todos`}</p>
+        )}
       </div>
-
     </div>
   );
-          }
+}
 
 export default App;
